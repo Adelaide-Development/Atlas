7629cdbcb7de48469dd3b11b8b98f381d8bcd7cf
Perseus <37126748+ajaidan@users.noreply.github.com>
2020-09-23 22:58:48 -0400

    oops

--- test.js
+++
@@ -1,4 +0,0 @@
-const { post } = require("./Atlas.js");
-require("./Atlas.js");
-
-post("How do I interface methods in Objective-C?", "Hello, everyone. I am a beginner in Objective-C and I always get errors about undefined class methods, and I've been told to interface them. Issue is, I don't know how. Can someone help me?");
