d14b0af15858495bbf200bf084b030bd06413bf0
Perseus <37126748+ajaidan@users.noreply.github.com>
2020-09-23 23:15:18 -0400

    brah

--- src/js/post.js
+++ src/js/post.js
@@ -13,18 +13,9 @@ const { sendToServer } = require("./sendToServer.js");
 exports.post = function () {
   let name = arguments[0];
   let content = arguments[1];
-  if (!name) {
-    return console.log(`[ATLAS] No name provided.`);
-  }
-  if (!content) {
-    return console.log(`[ATLAS] No content provided.`);
-  }
-  if (arguments[2]) {
-    return console.log(`[ATLAS] Unexpected identifier: "` + arguments[2] + `"`);
-  }
-  const output = JSON.stringify({
-    POST_NAME: name,
-    POST_CONTENT: content,
-  });
+  if (!name) { return console.log(`[ATLAS] No name provided.`); }
+  if (!content) { return console.log(`[ATLAS] No content provided.`); }
+  if (arguments[2]) { return console.log(`[ATLAS] Unexpected identifier: "` + arguments[2] + `"`); }
+  const output = JSON.stringify({ POST_NAME: name, POST_CONTENT: content });
   sendToServer(output);
 };
